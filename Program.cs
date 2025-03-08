52f39b3bd2a9a9f39960d76459474577a932460a
Amit Chandi <chandi.amit97@gmail.com>
2025-03-07 21:43:39 -0800

    /status and /wake API endpoints are functional

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,12 +16,41 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.MapGet("/status", () =>
+string targetIp = app.Configuration["TargetIp"] ?? throw new Exception("TargetIp is missing from config");
+string targetMac = app.Configuration["TargetMac"] ?? throw new Exception("TargetMac is missing from config");
+
+app.MapGet("/status", async (context) =>
 {
     // return some information about status of the server(s)
+    Ping pingSender = new Ping();
+    PingOptions options = new PingOptions();
+
+    // Use the default Ttl value which is 128,
+    // but change the fragmentation behavior.
+    options.DontFragment = true;
+
+    // Create a buffer of 32 bytes of data to be transmitted.
+    string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    byte[] buffer = Encoding.ASCII.GetBytes(data);
+    int timeout = 120;
+    PingReply reply = pingSender.Send(targetIp, timeout, buffer, options);
+
+    var resp = new
+    {
+        IsOn = reply.Status == IPStatus.Success
+    };
+
+    await context.Response.WriteAsJsonAsync(resp);
 })
 .WithName("GetStatus");
+
+app.MapGet("/wake", () =>
+{
+    Process.Start($"wakeonlan", $"{targetMac}");
+    return "Done";
+})
+.WithName("Wake");
 
 app.Run();
